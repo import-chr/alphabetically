7b46ca6ead21022a3b5f9e2129800ec1c78f2b3d
import-chr <yosoypro_117@hotmail.com>
2020-05-26 13:46:21 -0500

    Archivo py
    Se empezó a desarrollar el código en python

--- ordenar_dot_lang/python/ordenarAlfa.py
+++ ordenar_dot_lang/python/ordenarAlfa.py
@@ -1,1 +1,23 @@
-#archivo para ordenar alfabéticamente
+alfab = (
+    "a", "b", "c", "d", "e",
+    "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "ñ",
+    "o", "p", "q", "r", "s",
+    "t", "u", "v", "w", "x", "y", "z"
+)
+
+pOrdenadas = ""
+
+pPrueba = "hola ordename de manera alfebatica teniendo varias coincidencias alteradas para probar el algoritmo"
+arrPalabras = pPrueba.split()
+
+print(arrPalabras)
+
+def ordena(arr):
+    for palabra in arr:
+        if palabra[0] == alfab[0]:
+            print("ok")
+        else:
+                print("no")
+
+ordena(arrPalabras)
