6467db12529565d9750cdebd9032218fb6c37336
import-chr <yosoypro_117@hotmail.com>
2020-05-27 08:59:59 -0500

    Comparar arrays funcional

--- ordenar_dot_lang/python/ordenarAlfa.py
+++ ordenar_dot_lang/python/ordenarAlfa.py
@@ -11,9 +11,6 @@ p_ordenadas = ""
 p_prueba = "hola ordename de manera alfebatica teniendo varias coincidencias alteradas para probar el algoritmo"
 arr_palabras = p_prueba.split()
 arr_const = p_prueba.split()
-pos_letra = 0
-letra_alfa = 0
-n_pos_palabra = 0
 #print(arrPalabras)
 
 #cambia el index de los elementos
@@ -24,33 +21,41 @@ def cambiaIndex(arreglo, v_pos, n_pos):
 # print(arrPalabras)
 
 #retorna el index de un elemento
-def obtenIndex(arr1, pab):
+def obtener_index(arr1, pab):
     return arr1.index(pab)
 
-# inPab = obtenIndex(arrPalabras, "ordename")
+# inPab = obtener_index(arrPalabras, "ordename")
 # print(inPab)
 
 #compara arrays
-def comp_arrays(array0, array1, v_aumentar):
-    v_array0 = 0
-    v_array1 = 0
+def comp_arrays(array0, array1):
+    n_pos_palabra = 0
+    v_array = 0
 
-    if array0[v_array0] != array1[v_array1]:
-        v_aumentar += 1
+    for i in array0:
+        if array0[v_array] != array1[v_array]:
+            n_pos_palabra += 1
+            v_array += 1
+            # print(n_pos_palabra)
 
-    return v_aumentar
+    return n_pos_palabra
 
 #funcion principal, ordena el arreglo
 def ordena(arr):
+    pos_letra = 0
+    letra_alfa = 0
+
     for palabra in arr:
+        llama_obtener_index = obtener_index(arr, palabra)
+        llama_comp_arrays = comp_arrays(arr_const, arr_palabras)
         if palabra[pos_letra] == alfab[letra_alfa]:
-            cambiaIndex(arr, obtenIndex(arr, palabra), """numero""")
+            cambiaIndex(arr, llama_obtener_index, llama_comp_arrays)
             # print("ok")
         else:
             pass
             # print("no")
+
     return arr
 
 p1 = ordena(arr_palabras)
 print(p1)
-print(n_pos_palabra)
