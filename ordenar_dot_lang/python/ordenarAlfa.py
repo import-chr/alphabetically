75e08ea401f9d6e3382ebe9f78bd16b79e1f0733
import-chr <yosoypro_117@hotmail.com>
2020-05-28 21:47:04 -0500

    letra_alfa no incrementa
    letra_alfa no cincrementa de manera correcta

--- ordenar_dot_lang/python/ordenarAlfa.py
+++ ordenar_dot_lang/python/ordenarAlfa.py
@@ -1,9 +1,9 @@
 alfab = (
-    "a", "b", "c", "d", "e",
-    "f", "g", "h", "i", "j",
-    "k", "l", "m", "n", "ñ",
-    "o", "p", "q", "r", "s",
-    "t", "u", "v", "w", "x", "y", "z"
+	"a", "b", "c", "d", "e",
+	"f", "g", "h", "i", "j",
+	"k", "l", "m", "n", "ñ",
+	"o", "p", "q", "r", "s",
+	"t", "u", "v", "w", "x", "y", "z"
 )
 
 p_ordenadas = ""
@@ -15,47 +15,74 @@ arr_const = p_prueba.split()
 
 #cambia el index de los elementos
 def cambiaIndex(arreglo, v_pos, n_pos):
-    arreglo[n_pos], arreglo[v_pos] = arreglo[v_pos], arreglo[n_pos]
+	arreglo[n_pos], arreglo[v_pos] = arreglo[v_pos], arreglo[n_pos]
 
 # cambio = cambiaIndex(arrPalabras, 4, 0)
 # print(arrPalabras)
 
 #retorna el index de un elemento
 def obtener_index(arr1, pab):
-    return arr1.index(pab)
+	return arr1.index(pab)
 
 # inPab = obtener_index(arrPalabras, "ordename")
 # print(inPab)
 
 #compara arrays
 def comp_arrays(array0, array1):
-    n_pos_palabra = 0
-    v_array = 0
+	n_pos_palabra = 0
+	v_array = 0
 
-    for i in array0:
-        if array0[v_array] != array1[v_array]:
-            n_pos_palabra += 1
-            v_array += 1
-            # print(n_pos_palabra)
+	# si ya cambió una palabra, la nueva posición incrementa
+	for i in array0:
+		if array0[v_array] != array1[v_array]:
+			n_pos_palabra += 1
+			v_array += 1
+			# print(n_pos_palabra)
 
-    return n_pos_palabra
+	return n_pos_palabra
 
-#funcion principal, ordena el arreglo
+#ordena el arreglo
 def ordena(arr):
-    pos_letra = 0
-    letra_alfa = 0
+	# variables para iterar en arr y en sus index
+	letra_alfa = 0
+	pos_letra = 0
+	pabs = 0
+	len_arr = len(arr)
+	# print(len_arr)
 
-    for palabra in arr:
-        llama_obtener_index = obtener_index(arr, palabra)
-        llama_comp_arrays = comp_arrays(arr_const, arr_palabras)
-        if palabra[pos_letra] == alfab[letra_alfa]:
-            cambiaIndex(arr, llama_obtener_index, llama_comp_arrays)
-            # print("ok")
-        else:
-            pass
+
+	# cambia las palabras de arr si empiezan con letra_alfa
+	for palabra in arr:
+		llama_obtener_index = obtener_index(arr, palabra)
+		llama_comp_arrays = comp_arrays(arr_const, arr_palabras)
+
+		if palabra[pos_letra] == alfab[letra_alfa]:
+			cambiaIndex(arr, llama_obtener_index, llama_comp_arrays)
+			# print("ok")
+		else:
+			pass
             # print("no")
 
-    return arr
+		# letra_alfa no aumentó
+		if letra_alfa == 0:
+			print("letra_alfa no aumentó aquí, por lo que no busca la siguiente letra")
+
+	# aumenta letra_alfa si ya cambió todas las palabras que inician con letra_alfa
+	for i in range(len_arr):
+		if arr[pabs][0] == alfab[letra_alfa]:
+			pabs += 1
+			# print(pabs)
+		else:
+			letra_alfa += 1
+			# print(variable_aumentar)
+
+	# auxiliar, me dice si letra_alfa aumenta correctamente
+	if letra_alfa == 0:
+		print("el valor de letra_alfa es: " + str(letra_alfa) + " pero, no va a buscar demás letras")
+	else:
+		print("el valor de letra_alfa es: " + str(letra_alfa) + ", va a buscar demás letras")
+
+	return " ".join(arr)
 
 p1 = ordena(arr_palabras)
 print(p1)
